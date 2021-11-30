a74a5dc96ddfdddc493cc97ae23232d899dc4884
Tiffypox <tiffanyjaynedavies@hotmail.co.uk>
2021-11-30 14:20:56 +0000

    initial commit

---
+++ WpfTreeView/MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+
+namespace WpfTreeView
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        #region Constructor
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        #endregion
+    }
+}
